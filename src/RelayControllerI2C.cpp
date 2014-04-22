749e20ced7984a163ee53f7e2001dea832c0bd66
Nicolas <nicolas.aujoux@gmail.com>
2014-04-22 18:53:42 +0200

    Switch on with off

--- src/RelayControllerI2C.cpp
+++ src/RelayControllerI2C.cpp
@@ -11,29 +11,35 @@ RelayControllerI2C::RelayControllerI2C(uint8_t _i2cAddress, uint8_t _PCFPinNb)
     i2cAddress = _i2cAddress;
     PCFPinNb = _PCFPinNb;
 
-    off();
+    // off();
 }
 
 void RelayControllerI2C::on()
 {
-    uint8_t pinBit;
-    pinBit = 1 < PCFPinNb;
+    uint8_t pinBit = 0;
+    // pinBit = 1 < PCFPinNb;
 
     Wire.beginTransmission(i2cAddress);
-    Wire.write(pinBit); 
+    pinBit = Wire.read();
+    pinBit &= ~(1 << PCFPinNb); //this doesn't work ??
+    pinBit = 0; 
+    Wire.write(pinBit);
     Wire.endTransmission();
-    isRelayOn = 1;
+
+    isRelayOn = 0;
 }
 
 void RelayControllerI2C::off()
 {
-    uint8_t pinBit;
-    pinBit = 1 < PCFPinNb;
+    uint8_t pinBit = 0;
 
     Wire.beginTransmission(i2cAddress);
-    Wire.write(pinBit);
+    pinBit = Wire.read();
+    pinBit |= (1 << PCFPinNb);    
+    Wire.write(pinBit); 
     Wire.endTransmission();
-    isRelayOn = 0;
+
+    isRelayOn = 1;
 }
 
 uint8_t RelayControllerI2C::getRelayStatus()
